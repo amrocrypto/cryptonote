29a3ea1a94849482c24831464f0d054274b7b27d
amrocrypto <amrocrypto@gmail.com>
2017-02-22 01:41:53 +0200

    pointing seed nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "138.197.79.46:44444",
+  "45.55.241.1:44444",
 };
 
 struct CheckpointData {
